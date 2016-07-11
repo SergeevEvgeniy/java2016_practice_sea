3e4614e183c784c0c256c61d248e08806b53a7b4
Yauheni Siarheyeu <sea2512@yandex.ru>
2016-07-11 10:48:57 +0300

    task03 with little documentation

--- Car_maker_task/src/main/java/com/epam/cars/Output.java
+++ Car_maker_task/src/main/java/com/epam/cars/Output.java
@@ -3,8 +3,18 @@ package com.epam.cars;
 
 import com.epam.cars.model.Car;
 
+/**
+ * Class to output Car information
+ * 
+ * @author Æåíÿ
+ */
 public class Output {
 
+    /**
+     * Prints car information into console
+     * 
+     * @param car given car
+     */
     public void write(final Car car) {
         System.out.println(car.getMaker().getName() + " "
                 + car.getModel() + " "
