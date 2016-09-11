235d2a31dc1402492a91e66f05be393d2803b936
Yauheni Siarheyeu <sea2512@yandex.ru>
2016-09-11 15:06:04 +0300

    task09 mock-test

--- Car_maker_task/src/test/java/com/epam/cars/service/CarServiceTest.java
+++ Car_maker_task/src/test/java/com/epam/cars/service/CarServiceTest.java
@@ -1,16 +1,14 @@
 package com.epam.cars.service;
 
-import com.epam.cars.model.Car;
-import java.util.List;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
+import static org.mockito.Mockito.when;
 
-@Ignore
 public class CarServiceTest {
 
     public CarServiceTest() {
@@ -38,68 +36,12 @@ public class CarServiceTest {
     @Test
     public void testGetCars() {
         System.out.println("getCars");
-        CarService instance = new CarService();
-        List<Car> expResult = null;
-        List<Car> result = instance.getCars();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        CarService mock = new CarService();
 
-    /**
-     * Test of getCarsByModel method, of class CarService.
-     */
-    @Test
-    public void testGetCarsByModel() {
-        System.out.println("getCarsByModel");
-        String search = "";
-        CarService instance = new CarService();
-        List<Car> expResult = null;
-        List<Car> result = instance.getCarsByModel(search);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        ArrayList list = new ArrayList();
+        list.add(1);
+        when(mock.getCars()).thenReturn(list);
 
-    /**
-     * Test of saveCar method, of class CarService.
-     */
-    @Test
-    public void testSaveCar() {
-        System.out.println("saveCar");
-        Car car = null;
-        CarService instance = new CarService();
-        instance.saveCar(car);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(mock.getCars(), 1);
     }
-
-    /**
-     * Test of getCar method, of class CarService.
-     */
-    @Test
-    public void testGetCar() {
-        System.out.println("getCar");
-        Long id = null;
-        CarService instance = new CarService();
-        Car expResult = null;
-        Car result = instance.getCar(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateCar method, of class CarService.
-     */
-    @Test
-    public void testUpdateCar() {
-        System.out.println("updateCar");
-        Car car = null;
-        CarService instance = new CarService();
-        instance.updateCar(car);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
 }
